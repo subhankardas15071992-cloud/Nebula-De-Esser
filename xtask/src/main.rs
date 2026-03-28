97725472fd82e17abc5daae00a0e329114cc277c
Nebula Audio <subhankardas15071992@gmail.com>
2026-03-29 01:03:39 +0530

    Create main.rs

---
+++ xtask/src/main.rs
@@ -0,0 +1,5 @@
+// Delegate everything to nih_plug_xtask.
+// This one-liner is the standard pattern used by every nih-plug project.
+fn main() -> nih_plug_xtask::Result<()> {
+    nih_plug_xtask::main()
+}
